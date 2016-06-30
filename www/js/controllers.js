4c35f3db8234392daba781e37b87e1f4bb8c4857
malkitsingh <malkitsingh01@gmail.com>
2016-06-30 16:54:37 +0530

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,11 @@
+﻿angular.module('starter.controllers', [])
+    .controller('LoginCtrl', function ($scope) {
+
+        $scope.$on("$ionicView.enter", function (event, data) {
+            // handle event
+            console.log('Login Controller loaded');
+            swal("Here's a message!");
+        });
+
+        
+    });
