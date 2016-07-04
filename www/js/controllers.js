7886c851d72d70131a3fafb16379df8b315c75b4
surya <suryaprakash9@hotmail.com>
2016-07-04 17:02:18 +0530

    Added validation for login page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,26 +4,23 @@
         $scope.$on("$ionicView.enter", function (event, data) {
             // handle event
             console.log('Login Controller loaded');
-           // swal("Here's a message!");
+            // swal("Here's a message!");
         });
 
         $scope.SignIn = function (username, pwd) {
-         
-          
+            if ($('#frmLogin').parsley().validate() == true) {
+                authenticationService.Login(username, pwd, '', function (response) {
+                    console.log(response.Result + ',' + response.Result.indexOf('Temporarily_Blocked'));
 
-            authenticationService.Login(username, pwd, '', function (response) {
-
-                console.log(response.Result + ',' + response.Result.indexOf('Temporarily_Blocked'));
-                
-                if (response.Result.indexOf('Invalid ') > -1 || response.Result.indexOf('incorrect ') > -1 || response.Result.indexOf('Temporarily_Blocked') > -1) {
-                    swal(response.Result);
-                }
-                else {
-                    swal("login successfull");
-                }
-            });
+                    if (response.Result.indexOf('Invalid ') > -1 || response.Result.indexOf('incorrect ') > -1 || response.Result.indexOf('Temporarily_Blocked') > -1) {
+                        swal(response.Result);
+                    }
+                    else {
+                        swal("login successfull");
+                    }
+                });
+            }
         }
-        
     })
 
 .controller('SignupCtrl', function ($scope) {
@@ -32,8 +29,8 @@
         // handle event
         console.log('Signup Controller loaded');
         // swal("Here's a message!");
-       
- 
+
+
     });
 
 
