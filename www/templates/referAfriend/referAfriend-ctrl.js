2f8f4dbf8dc254926259df67257f136f6871a3b3
Cliff Canan <cliff@nooch.com>
2016-07-16 17:27:52 -0400

    • Added Social Sharing Cordova Plugin

--- www/templates/referAfriend/referAfriend-ctrl.js
+++ www/templates/referAfriend/referAfriend-ctrl.js
@@ -7,11 +7,14 @@
      });
 
      $ionicPlatform.ready(function () {
-         window.plugins.spinnerDialog.show("title", "message", true);
+         //window.plugins.spinnerDialog.show("title", "message", true);
      });
 
      $scope.sendReferralCode = function (type) {
          console.log(type + " clicked");
+
+         var imageURL = "http://noochme.com/noochweb/Assets/Images/noochlogosquare.png";
+         var addUrl = "http://bit.ly/1xdG2le";
 
          if (type == "sms")
          {
@@ -20,7 +23,7 @@
              $ionicPlatform.ready(function () {
                  // access multiple numbers in a string like: '0612345678,0687654321'
                  $cordovaSocialSharing
-                   .shareViaSMS(message, number)
+                   .shareViaSMS(msg, "6108041572") // Cliff's # JUST FOR TESTING
                    .then(function (result) {
                        // Success!
                    }, function (err) {
@@ -30,13 +33,11 @@
          }
          else if (type == "fb")
          {
-             var shareTitle = "Check Out Nooch!";
              var shareDesc = "Check out Nooch, the simplest way to pay me back (and get paid by anyone - for free)! Use my invite code to sign up: \"" + $scope.inviteCode + "\"";
-             var imageURL = "http://noochme.com/noochweb/Assets/Images/noochlogosquare.png";
 
              $ionicPlatform.ready(function () {
                  $cordovaSocialSharing
-                    .shareViaFacebook(message, image, link)
+                    .shareViaFacebook(shareDesc, imageURL, addUrl)
                     .then(function (result) {
                         // Success!
                     }, function (err) {
@@ -47,11 +48,10 @@
          else if (type == "twitter")
          {
              var tweetTxt = "Check out @NoochMoney, the simplest free way to pay me back! Use my invite code to sign up: \"" + $scope.inviteCode + "\"";
-             var addUrl = "http://bit.ly/1xdG2le";
 
              $ionicPlatform.ready(function () {
                  $cordovaSocialSharing
-                    .shareViaTwitter(message, image, link)
+                    .shareViaTwitter(tweetTxt, image, addUrl)
                     .then(function (result) {
                         // Success!
                     }, function (err) {
@@ -61,13 +61,16 @@
          }
          else if (type == "email")
          {
-             var emailTitle = "Check out Nooch - a free app to pay me back";
+             var subject = "Check out Nooch - a free app to pay me back";
              var msgBody = "Hey there,<br/><p>You should check out Nooch, a great <strong>free app</strong> that lets me pay you back anytime, anywhere.  Since I know you don't like carrying cash around either, I thought you would love using Nooch!</p><p>You can <a href=\"https://157050.measurementapi.com/serve?action=click&publisher_id=157050&site_id=91086\">download Nooch</a> from the App Store - and be sure to use my Referral Code:</p><p style=\"text-align:center;font-size:1.5em;\"><strong>" + $scope.inviteCode + "</strong></p><p>To learn more about Nooch, here's the website: <a href=\"https://www.nooch.com/overview/\">www.Nooch.com</a>.</p><p>- " + $scope.firstName + "</p>";
+             var toArr = ["cliff@nooch.com"]; // Just for testing
+             var ccArr = [""];
+             var bccArr = ["cliff@nooch.com"];
 
              $ionicPlatform.ready(function () {
                  // toArr, ccArr and bccArr must be an array, file can be either null, string or array
                  $cordovaSocialSharing
-                   .shareViaEmail(message, subject, toArr, ccArr, bccArr, file)
+                   .shareViaEmail(msgBody, subject, toArr, ccArr, bccArr, null)
                    .then(function (result) {
                        // Success!
                    }, function (err) {
