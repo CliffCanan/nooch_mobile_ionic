7573eb04f16796532a1ef66e24d0f04069ec8992
rajat puri <rajat puri>
2016-07-26 11:42:16 +0530

    added modal pop up for geting user pin before making request money transaction

--- www/templates/howMuch/howMuch-ctrl.js
+++ www/templates/howMuch/howMuch-ctrl.js
@@ -2,11 +2,11 @@
 
 .controller('howMuchCtrl', function ($scope, $state, $ionicPlatform, $ionicHistory, $stateParams, $ionicModal, howMuchService, $localStorage, $ionicPopup, CommonServices, ValidatePin, $ionicLoading) {
 
-    $ionicModal.fromTemplateUrl('templates/enterPinForeground/enterPinForeground.html', {
+    $ionicModal.fromTemplateUrl('templates/howMuch/modalPin.html', {
         scope: $scope
     }).then(function (modal) {
         $scope.modal = modal;
-        
+
     });
 
     $scope.requestData = {
@@ -68,72 +68,68 @@
         
         if ($('#howMuchForm').parsley().validate() == true)
         {
-            var myPopup = $ionicPopup.show({
-                template: '<input type="password" ng-model="recipientDetail.pin">',
-                title: 'Enter Pin',
-                subTitle: 'Please enter Nooch Pin',
-                scope: $scope,
-                buttons: [
-                  
-                  {
-                      text: '<b>Done</b>',
-                      type: 'button-positive',
-                      onTap: function (e) {
-                          
-                          if (!$scope.recipientDetail.pin) {
-                              //don't allow the user to close unless he enters wifi password
-                              e.preventDefault();
-                          } else {
-                              console.log($scope);
-                              return $scope.recipientDetail.pin;
-                          }
-                      }
-                  }
-                ]
-            });
+            $scope.modal.show();
 
-            myPopup.then(function (res) {
-                CommonServices.GetEncryptedData(res).success(function (data) {
-                    console.log(data.Status);
-
-                    ValidatePin.ValidatePinNumberToEnterForEnterForeground(data.Status)
-                   .success(function (data) {
-                       // $scope.Data = data;
-                       console.log($scope.data);
-
-                       $ionicLoading.hide();
-                       if (data.Result == 'Success') {
-                           console.log($scope.recipientDetail.Amount);
-                           $scope.requestData.MemberId = $localStorage.GLOBAL_VARIABLES.MemberId;
-                           $scope.requestData.Amount = $scope.recipientDetail.Amount;
-                           $scope.requestData.SenderId = $scope.recipientDetail.MemberId;
-                           $scope.requestData.Name = $scope.recipientDetail.FirstName + ' ' + $scope.recipientDetail.LastName;
-                           $scope.requestData.Memo = $scope.recipientDetail.Memo;
-                           howMuchService.RequestMoney($scope.requestData).success(function (data) {
-
-                               if (data.Result.indexOf('successfully') > -1) {
-                                   swal("Success...", data.Result, "success");
-                               }
-                               else {
-                                   swal("Error...", data.Result, "error");
-                               }
-                           }).error();
-                       }
-                       else if (data.Result == 'Invalid Pin') {
-                           swal("Oops...", "Incorrect Pin !", "error");
-                       }
-                       else if (data.Message == 'An error has occurred.') {
-                           swal("Oops...", "Something went wrong !", "error");
-                       }
-
-                   }).error(function (data) {
-                       console.log('eror' + data);
-                       $ionicLoading.hide();
-                   });
-                }).error(function (data) { });
-            });
+       
           
            // $state.go('enterPin');
+        }
+    };
+
+
+    $scope.CheckPin = function (Pin) {
+        console.log('Check Pin Function called');
+        if ($('#frmPinForeground').parsley().validate() == true) {
+            $scope.modal.hide();
+            console.log(Pin);
+
+            //if ($cordovaNetwork.isOnline()) {
+            $ionicLoading.show({
+                template: 'Loading ...'
+            });
+
+            CommonServices.GetEncryptedData(Pin).success(function (data) {
+                console.log(data.Status);
+                $scope.requestData.PinNumber = data.Status;
+                ValidatePin.ValidatePinNumberToEnterForEnterForeground(data.Status)
+               .success(function (data) {
+                   // $scope.Data = data;
+                   console.log($scope.data);
+
+                   $ionicLoading.hide();
+                   if (data.Result == 'Success') {
+                       console.log($scope.recipientDetail.Amount);
+                       $scope.requestData.MemberId = $localStorage.GLOBAL_VARIABLES.MemberId;
+                       $scope.requestData.Amount = $scope.recipientDetail.Amount;
+                       $scope.requestData.SenderId = $scope.recipientDetail.MemberId;
+                       $scope.requestData.Name = $scope.recipientDetail.FirstName + ' ' + $scope.recipientDetail.LastName;
+                       $scope.requestData.Memo = $scope.recipientDetail.Memo;
+                       howMuchService.RequestMoney($scope.requestData).success(function (data) {
+
+                           if (data.Result.indexOf('successfully') > -1) {
+                               swal("Success...", data.Result, "success");
+                           }
+                           else {
+                               swal("Error...", data.Result, "error");
+                           }
+                       }).error();
+                   }
+                   else if (data.Result == 'Invalid Pin') {
+                       swal("Oops...", "Incorrect Pin !", "error");
+                   }
+                   else if (data.Message == 'An error has occurred.') {
+                       swal("Oops...", "Something went wrong !", "error");
+                   }
+
+               }).error(function (data) {
+                   console.log('eror' + data);
+                   $ionicLoading.hide();
+               });
+            }).error(function (data) { });
+            //}
+            //else {
+            //    swal("Oops...", "Internet not connected!", "error");
+            //}
         }
     };
  
