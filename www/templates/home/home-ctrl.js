a6e4d6bffbece0bedbf7feeda7f42672ccd11450
rajat puri <rajat puri>
2016-07-27 17:22:02 +0530

    added plugins for camera , google analytics and image picker

--- www/templates/home/home-ctrl.js
+++ www/templates/home/home-ctrl.js
@@ -4,14 +4,35 @@
 /****************/
 /***   HOME   ***/
 /****************/
-.controller('HomeCtrl', function ($scope, $state, authenticationService) {
+.controller('HomeCtrl', function ($scope, $state, authenticationService, $cordovaGoogleAnalytics) {
 
     
  
 
     $scope.$on("$ionicView.enter", function (event, data) {
         console.log('Home Ctrl loaded');
+        console.log($cordovaGoogleAnalytics);
+        $cordovaGoogleAnalytics.debugMode();
+        $cordovaGoogleAnalytics.setUserId('USER_ID');
+ 
+
+        $cordovaGoogleAnalytics.trackView('Home Screen');
+
+      
+
+        // track event
         
+        $cordovaGoogleAnalytics.trackEvent('', '', '', 100);
+
+        // add transaction
+        
+        $cordovaGoogleAnalytics.addTransaction('', '', '', '5', '1.29', 'EUR');
+
+        // add transaction item
+        
+        $cordovaGoogleAnalytics.addTransactionItem(
+          '1234', ' ', 'DD23444', ' ', '11.99', '1', 'GBP'
+        );
          
     });
 
