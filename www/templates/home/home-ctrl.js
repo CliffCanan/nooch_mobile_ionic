e919d354268da2ceb94e7ef9411beb5581d124a3
rajat puri <rajat puri>
2016-07-28 11:10:35 +0530

    changes in howmuch controller to send picture and re install missing plugins

--- www/templates/home/home-ctrl.js
+++ www/templates/home/home-ctrl.js
@@ -4,15 +4,22 @@
 /****************/
 /***   HOME   ***/
 /****************/
-.controller('HomeCtrl', function ($scope, $state, authenticationService) {
+.controller('HomeCtrl', function ($scope, $state, authenticationService, $cordovaGoogleAnalytics) {
 
     
  
 
     $scope.$on("$ionicView.enter", function (event, data) {
         console.log('Home Ctrl loaded');
-        
-         
+        $cordovaGoogleAnalytics.debugMode();
+        $cordovaGoogleAnalytics.startTrackerWithId('UA-000000-01');
+        $cordovaGoogleAnalytics.setUserId('USER_ID');
+        $cordovaGoogleAnalytics.trackView('Home Screen');
+        $cordovaGoogleAnalytics.addTransaction('1234', 'Testing', '11.99', '5', '1.29', 'EUR');
+        $cordovaGoogleAnalytics.addTransactionItem(
+    '1234', 'Testing', 'DD23444', 'Testing', '11.99', '1', 'GBP'
+  );
+        console.log($cordovaGoogleAnalytics);
     });
 
     $scope.goToSelectRecip = function () {
--- www/templates/howMuch/howMuch-ctrl.js
+++ www/templates/howMuch/howMuch-ctrl.js
@@ -1,6 +1,6 @@
 ﻿angular.module('noochApp.howMuchCtrl', ['ngCordova', 'noochApp.howMuch-service', 'noochApp.services'])
 
-.controller('howMuchCtrl', function ($scope, $state, $ionicPlatform, $ionicHistory, $stateParams, $ionicModal, howMuchService, $localStorage, $ionicPopup, CommonServices, ValidatePin, $ionicLoading, $cordovaCamera) {
+.controller('howMuchCtrl', function ($scope, $state, $ionicPlatform, $ionicHistory, $stateParams, $ionicModal, howMuchService, $localStorage, $ionicPopup, CommonServices, ValidatePin, $ionicLoading, $cordovaCamera, $cordovaImagePicker) {
 
     
 
@@ -220,7 +220,7 @@
                            $scope.requestData.SenderId = $scope.recipientDetail.MemberId;
                            $scope.requestData.Name = $scope.recipientDetail.FirstName + ' ' + $scope.recipientDetail.LastName;
                            $scope.requestData.Memo = $scope.recipientDetail.Amount;
-                           $scope.requestData.Picture = $scope.imgURI;
+                           $scope.requestData.Picture = $scope.picture;
                            howMuchService.RequestMoney($scope.requestData).success(function (data) {
 
                                if (data.Result.indexOf('successfully') > -1) {
@@ -241,7 +241,7 @@
                            $scope.sendData.RecepientId = $scope.recipientDetail.MemberId;
                            $scope.sendData.RecepientName = $scope.recipientDetail.FirstName + ' ' + $scope.recipientDetail.LastName;
                            $scope.sendData.Memo = $scope.recipientDetail.Amount;
-                           $scope.sendData.Picture = $scope.imgURI;
+                           $scope.sendData.Picture = $scope.picture;
                            howMuchService.TransferMoney($scope.sendData).success(function (data) {
                                    if (data.Result && data.Result.indexOf('Successfully') > -1) {
                                        swal("Payed...", data.Result, "success");
@@ -283,25 +283,44 @@
 
 
         $ionicPlatform.ready(function () {
-            console.log(window);
-            window.imagePicker.getPictures(function (results) {
-                for (var i = 0; i < results.length; i++)
-                {
-                    console.log('Image URI: ' + results[i]);
-                }
-            }, function (error) {
-                console.log('Error: ' + error);
-            }, {
-                maximumImagesCount: 1,
-                width: 800
-            }
-            );
+            
+            //window.imagePicker.getPictures(function (results) {
+            //    for (var i = 0; i < results.length; i++)
+            //    {
+            //        console.log('Image URI: ' + results[i]);
+            //    }
+            //}, function (error) {
+            //    console.log('Error: ' + error);
+            //}, {
+            //    maximumImagesCount: 1,
+            //    width: 800
+            //}
+            //);
+
+            var options = {
+                maximumImagesCount: 10,
+                width: 800,
+                height: 800,
+                quality: 80
+            };
+            console.log($cordovaImagePicker);
+            $cordovaImagePicker.getPictures(options)
+              .then(function (results) {
+                  for (var i = 0; i < results.length; i++) {
+                      console.log('Image URI: ' + results[i]);
+                  }
+              }, function (error) {
+                  // error getting photos
+              });
+
         });
     };
 
 
     $scope.takePhoto = function () {
         console.log($cordovaCamera);
+
+        $ionicPlatform.ready(function () {
         var options = {
             quality: 75,
             destinationType: Camera.DestinationType.DATA_URL,
@@ -317,13 +336,16 @@
         $cordovaCamera.getPicture(options).then(function (imageData) {
             console.log(imageData);
             $scope.imgURI = "data:image/jpeg;base64," + imageData;
+            $scope.picture = imageData;
         }, function (err) {
             // An error occured. Show a message to the user
+        });
         });
     }
 
 
     $scope.choosePhoto = function () {
+        $ionicPlatform.ready(function () {
         var options = {
             quality: 75,
             destinationType: Camera.DestinationType.DATA_URL,
@@ -341,6 +363,7 @@
             $scope.imgURI = "data:image/jpeg;base64," + imageData;
         }, function (err) {
             // An error occured. Show a message to the user
+        });
         });
     }
 })
