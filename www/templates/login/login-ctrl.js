48cb955afabb1001074203037361627f61989024
Cliff Canan <cliff@nooch.com>
2016-08-03 04:07:59 -0400

    • Layout fixes for addPicture and CreatePin screens

--- www/templates/login/login-ctrl.js
+++ www/templates/login/login-ctrl.js
@@ -85,14 +85,14 @@
                         console.log(response.Result + ', ' + response.Result.indexOf('Temporarily_Blocked'));
                         console.log(response);
 
+                        $ionicLoading.hide();
+
                         if (response.Result.indexOf('Invalid') > -1 || response.Result.indexOf('incorrect') > -1)
                         {
-                            $ionicLoading.hide();
                             swal(response.Result);
                         }
                         else if (response.Result.indexOf('Temporarily_Blocked') > -1)
                         {
-                            $ionicLoading.hide();
                             swal({
                                 title: "Oh No!",
                                 text: "To keep Nooch safe your account has been temporarily suspended because you entered an incorrect passwod too many times.<br><br> In most cases your account will be automatically un-suspended in 24 hours. you can always contact support if this is an error.<br><br> We really apologize for the inconvenience and ask for your patience. Our top priority is keeping Nooch safe and secure.",
@@ -123,7 +123,8 @@
                         }
                     }
                     ).error(function (res) {
-                        console.log('Login Attempt Error: [' + res + ']');
+                        $ionicLoading.hide();
+                        console.log('Login Attempt Error: [' + JSON.stringify(res) + ']');
                     });
               }).error(function (encError) {
                   console.log('came in enc error block ' + encError);
